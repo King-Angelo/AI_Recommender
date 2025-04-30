a9276d1d500ed9ced2c372a68ca28fc5ed38636d
King Mangahas <mkamangahas@tip.edu.ph>
2025-04-30 11:54:54 +0800

    Add new Render domain to ALLOWED_HOSTS

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -25,8 +25,16 @@ SECRET_KEY = 'django-insecure-2vd^9@!w7t%r(!!@s2+%sis1f@4n-78+enc1_=1=9fqbrmf6v)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+import os
 
+# Environment variables
+SECRET_KEY = os.getenv('ggxliadXip6nyw9lh8kuxz4q8h-lypHey6g+jd2f2syr3Xy#12', "fallback_dev_secret")
+DEBUG = False
+ALLOWED_HOSTS = ['ai-recommender-wbjp.onrender.com', 'ai-recommender-k0wy.onrender.com', 'localhost', '127.0.0.1']
+
+OPENAI_KEY = os.getenv("sk-proj-0123456789abcdef0123456789abcdef")
+MONGO_URI = os.getenv("vu#j$zf83slu1%-duw04g85_cs2+l%%m^_g1flthiwr4u5hr9_")
+# Add your environment variables for MongoDB and OpenAI    You, 34 minutes ago - add requirements.
 
 # Application definition
 
